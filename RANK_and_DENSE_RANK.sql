88a81b38d6c28b7ecfc853a697056dc1dc8dceb1
maxbicking <113531787+maxbicking@users.noreply.github.com>
2022-12-10 10:40:22 -0500

    Add files via upload

---
+++ RANK_and_DENSE_RANK.sql
@@ -0,0 +1,79 @@
+/*
+Write a query that lists each product, category, and subcategory; ranks overall list price, as well
+as price within each category, and determines if the product is one ot the top 5 most expensive items. 
+*/
+
+SELECT 
+  ProductName = A.Name,
+  A.ListPrice,
+  ProductSubcategory = B.Name,
+  ProductCategory = C.Name,
+  [Price Rank] = ROW_NUMBER() OVER(ORDER BY A.ListPrice DESC),
+  [Category Price Rank] = ROW_NUMBER() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Category Price Rank With Rank] = RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Top 5 Price In Category] = 
+	CASE 
+		WHEN ROW_NUMBER() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC) <= 5 THEN 'Yes'
+		ELSE 'No'
+	END
+FROM AdventureWorks2019.Production.Product A
+  JOIN AdventureWorks2019.Production.ProductSubcategory B
+    ON A.ProductSubcategoryID = B.ProductSubcategoryID
+  JOIN AdventureWorks2019.Production.ProductCategory C
+    ON B.ProductCategoryID = C.ProductCategoryID
+
+
+
+/*
+Modify the previous query and use DENSE_RANK instead of RANK
+*/
+
+SELECT 
+  ProductName = A.Name,
+  A.ListPrice,
+  ProductSubcategory = B.Name,
+  ProductCategory = C.Name,
+  [Price Rank] = ROW_NUMBER() OVER(ORDER BY A.ListPrice DESC),
+  [Category Price Rank] = ROW_NUMBER() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Category Price Rank With Rank] = RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Category Price Rank With Dense Rank] = DENSE_RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Top 5 Price In Category] = 
+	CASE 
+		WHEN ROW_NUMBER() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC) <= 5 THEN 'Yes'
+		ELSE 'No'
+	END
+FROM AdventureWorks2019.Production.Product A
+  JOIN AdventureWorks2019.Production.ProductSubcategory B
+    ON A.ProductSubcategoryID = B.ProductSubcategoryID
+  JOIN AdventureWorks2019.Production.ProductCategory C
+    ON B.ProductCategoryID = C.ProductCategoryID
+
+
+
+/*
+Return a true top 5 products by price, assuming we want to see the 
+top 5 distinct prices AND we want “ties” (by price) to all share the same rank
+*/
+
+SELECT 
+  ProductName = A.Name,
+  A.ListPrice,
+  ProductSubcategory = B.Name,
+  ProductCategory = C.Name,
+  [Price Rank] = ROW_NUMBER() OVER(ORDER BY A.ListPrice DESC),
+  [Category Price Rank] = ROW_NUMBER() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Category Price Rank With Rank] = RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Category Price Rank With Dense Rank] = DENSE_RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC),
+  [Top 5 Price In Category] = 
+	CASE 
+		WHEN DENSE_RANK() OVER(PARTITION BY C.Name ORDER BY A.ListPrice DESC) <= 5 THEN 'Yes'
+		ELSE 'No'
+	END
+FROM AdventureWorks2019.Production.Product A
+  JOIN AdventureWorks2019.Production.ProductSubcategory B
+    ON A.ProductSubcategoryID = B.ProductSubcategoryID
+  JOIN AdventureWorks2019.Production.ProductCategory C
+    ON B.ProductCategoryID = C.ProductCategoryID
+
+
+
